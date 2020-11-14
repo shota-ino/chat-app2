93fdb0551872888ecc9c4a4b9e6468b576ce0d17
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-14 10:46:06 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatApp2
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    
+    # 必要のないファイルですので、設定を変更（追記）して生成させない
+    config.generators do |g|
+      # 生成したファイルに対応したスタイルシート
+      g.stylesheets false
+      # 生成したファイルに対応したJavaScriptファイル
+      g.javascripts false
+      # 生成したファイルに対応したヘルパー
+      g.helper false
+      # アプリケーションをテストするためのファイル
+      g.test_framework false
+    end
+  end
+end
