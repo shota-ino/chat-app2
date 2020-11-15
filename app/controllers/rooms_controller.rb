2bacb715b218a73144304229d7c48826f8f5185b
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-15 17:58:54 +0900

    チャットルーム作成機能の実装
    bundle install
    プルダウン
    からのカレントユーザーと他のユーザーでのルーム作成（4711参照）

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,4 +5,31 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    # binding.pry その後Cでparams
+    # プルダウンを利用したときにどのようなデータが送信されるのか確認
+    # "room"=>{"name"=>"新規チャット", "user_ids"=>["1", "2"]}に注目
+    # カリキュラム4711の『送信されるデータを確認しましょう』を参照
+
+    @room = Room.new(room_params)
+    # 保存が成功したかどうかで処理を分岐
+    if @room.save
+      # 成功した場合はredirect_toメソッドでルートパスにリダイレクト
+      redirect_to root_path
+    else
+      # 失敗した場合はrenderメソッドでrooms/new.html.erbのページを表示
+      render :new
+    end
+
+  end
+
+  private
+
+  def room_params
+    # 配列として受け取ったデータを含むストロングパラメーターを定義
+                  # 配列に対して保存を許可したい場合は、キーに対し[]を値として記述# 
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
+
 end
