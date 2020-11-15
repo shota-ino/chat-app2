db7781fbba0be3fe49cc3be15b6ddcb70463b2b6
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 07:54:09 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,10 @@ class MessagesController < ApplicationController
     # paramsに含まれているroom_idを代入
                     # params[:room_id]と記述することでroom_idを取得できる
     @room = Room.find(params[:room_id])
+    # チャットルームに紐付いている全てのメッセージをを定義し、ユーザー情報も紐付いて表示させる
+        # メッセージに紐付くユーザー情報の取得に、メッセージの数と同じ回数のアクセスが必要になるので、N+1問題が発生
+        # 全てのメッセージ情報に紐づくユーザー情報を、includes(:user)と記述をすることにより、ユーザー情報を1度のアクセスでまとめて取得することができる
+    @messages = @room.messages.includes(:user)
   end
 
 
@@ -21,6 +25,8 @@ class MessagesController < ApplicationController
     else
       # 保存に失敗した場合、同じページに戻る
       render :index
+      # @messagesが定義されていないとエラーになってしまうため、indexアクションと同様に@messagesを定義
+      @messages = @room.messages.includes(:user)
     end
   end
 
