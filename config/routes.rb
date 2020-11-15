4005d4a724691fd91c9af793eb917c6ea2c74106
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-15 16:38:15 +0900

    ユーザー編集機能の実装
    rails g controller users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   # ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
   root to: "messages#index"
+  # ユーザー編集に必要なルーティング
+  resources :users, only: [:edit, :update]
 end
